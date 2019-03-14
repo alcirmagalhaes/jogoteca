90ae85c227eec7cf99b98974d18913559be47af6
alcir <alcirmagalhaes@gmail.com>
2019-03-14 20:35:14 -0300

    aula 1

---
+++ jogoteca.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/inicio')
+def ola():
+    # return ('<h1> teste </h1>')
+    return render_template('lista.html')
+
+
+app.run()
