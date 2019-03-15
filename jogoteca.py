3670ba5c136015124308f70d943adf4c116ab0a7
alcir <alcirmagalhaes@gmail.com>
2019-03-15 19:34:05 -0300

    criação da rota 'criar' responsável por capturar o submit do form da pagina novo.html e gerar com base no request um novo jogo na listagem de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -9,18 +10,29 @@ class Jogo:
 
 app = Flask(__name__)
 
+jogo1 = Jogo('fifa 2019', 'esporte', 'play station')
+jogo2 = Jogo('god of war', 'mitologia', 'play station')
+jogo3 = Jogo('mortal kombat', 'luta', 'play station')
+lista = [jogo1, jogo2, jogo3]
 
 @app.route('/inicio')
 def ola():
-    jogo1 = Jogo('fifa 2019', 'esporte', 'play station')
-    jogo2 = Jogo('god of war', 'mitologia', 'play station')
-    jogo3 = Jogo('mortal kombat', 'luta', 'play station')
-    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogo', jogos=lista)
 
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar')
+def criar ():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
