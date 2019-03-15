196e969c009b109251de319602ac3f4ff0883773
alcir <alcirmagalhaes@gmail.com>
2019-03-15 20:09:27 -0300

    redirecionar a página após a criação do jogo para a listagem

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 class Jogo:
@@ -35,7 +35,7 @@ def criar ():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
 
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+    return redirect('/')
 
 
 app.run(debug=True)
