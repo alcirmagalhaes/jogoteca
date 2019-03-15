f97b23ef932ec63d61a32e4e424cb3db25b3e1e4
alcir <alcirmagalhaes@gmail.com>
2019-03-15 17:15:13 -0300

    titulo da página agora é dinâmico

--- jogoteca.py
+++ jogoteca.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/inicio')
 def ola():
     # return ('<h1> teste </h1>')
-    return render_template('lista.html')
+    return render_template('lista.html', titulo='Jogo')
 
 
 app.run()
