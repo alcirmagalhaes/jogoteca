142ab8711600cd5b6ef30c995764bbcbaf3d6440
alcir <alcirmagalhaes@gmail.com>
2019-03-15 17:38:28 -0300

    criação da classe jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    lista = ['tetris', 'fifa 2019', 'god of war', 'metal gear solid']
+    jogo1 = Jogo('fifa 2019', 'esporte', 'play station')
+    jogo2 = Jogo('god of war', 'mitologia', 'play station')
+    lista = [jogo1, jogo2]
     return render_template('lista.html', titulo='Jogo', jogos=lista)
 
 
